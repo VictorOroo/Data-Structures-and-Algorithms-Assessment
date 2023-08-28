8bc4d48c050b496fb2dc1513d3b9421b12b833f0
victoronyonkaoroo <victor.oroo@student.moringaschool.com>
2023-08-28 20:29:49 +0300

    updated code

--- sequences.py
+++ sequences.py
@@ -1,9 +1,9 @@
 def remove_duplicates(sequence):
-    unique_elements = []
+    result_list = []
     for item in sequence:
-        if item not in unique_elements:
-            unique_elements.append(item)
-    return unique_elements
+        if item not in result_list:
+            result_list.append(item)
+    return result_list
 
 input_sequence = [2, 3, 2, 4, 5, 3, 6, 7, 5]
 result = remove_duplicates(input_sequence)
